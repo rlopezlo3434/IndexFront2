ef19665c3fb2757e095d0ed1e0b18880479c0b44
Renzo <152234907+rlopezlo3434@users.noreply.github.com>
2024-11-03 09:01:09 -0500

    .

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',  // URL de la API en desarrollo
+    // apiUrl: 'http://localhost:3000/api',  // URL de la API en desarrollo
+    apiUrl: 'https://indexapi-production.up.railway.app/api',
     anotherVariable: 'valor-desarrollo'   // Otras variables que puedas necesitar
   };
   
